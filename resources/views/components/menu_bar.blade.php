266631f2e9e50217021f080c6c1564425c828790
ShogoAkiyama54 <sakiyama@knox.edu>
2019-09-22 15:02:40 -0500

    Add contents in Array Page

--- resources/views/components/menu_bar.blade.php
+++ resources/views/components/menu_bar.blade.php
@@ -5,30 +5,33 @@
     <ul>
         <li>
             <div>
-                <a href='/'>Home</a>
+                <h3>Home</h3>
                 <ul>
+                    <li><a href='/'>Welcome</a></li>
                     <li><a href='/about'>About</a></li>
-                    <li><a href='/how-to-use'>How to use</a></li>
+                    <li><a href='/how-to-use'>How to Use</a></li>
                     <li><a href='/contact'>Contact</a></li>
                 </ul>
             </div>
         </li>
         <li>
             <div>
-                Object Oriented Programming
+                <h3>Object Oriented Programming</h3>
                 <ul>
-                    <li><a href='/about'>class & instance</a></li>
+                    <li><a href='/about'>Class & Instance</a></li>
                     <li><a href='/how-to-use'>3 components</a></li>
-                    <li><a href='/contact'>inheritance</a></li>
+                    <li><a href='/contact'>Inheritance</a></li>
                 </ul>
             </div>
         </li>
         <li>
             <div>
-                <h5>Data Structures</h5>
+                <h3>Data Structures</h3>
                 <ul>
                     <li><a href='/data-structure/array'>Array</a></li>
-                    <li><a href='/data-structure/list'>List</a></li>
+                    <li><a href='/data-structure/array-list'>Array List</a></li>
+                    <li><a href='/data-strucutre/linked-list'>Linked List</a></li>
+                    <li><a href='/data-structure/list'>Overview of List</a></li>
                 </ul>
             </div>
 
--- resources/views/data_structure/array.blade.php
+++ resources/views/data_structure/array.blade.php
@@ -3,15 +3,20 @@
 @section('content')
     <h1>Array</h1>
 
+    <div>
+        <h3>Table of Contents</h3>
+
+    </div>
+
     <h2>Inconvenience of Variables</h2>
     <p>
-        As a programmer, you often face to a situation where you need to deal with lots of data at once.
-        Sometimes you may need to know the average of an class exam score.
+        As a programmer, we often face to a situation where we need to deal with lots of data at once.
+        Sometimes we may need to know the average of an class exam score.
     </p>
     <p>
-        Of course, you can have variables for each person's score, sum them up, and divide by the number of classmates.
+        Of course, we can have variables for each person's score, sum them up, and divide by the number of classmates.
         However, the code for that will look tedious.
-        Not only that, but also you need to add more and more lines of code as you have more student in your class.
+        Not only that, but also we need to add more and more lines of code as we have more student in our class.
     </p>
     <p class='code'>
         int jack = 77;<br>
@@ -25,13 +30,13 @@
         int avg = sum / 6;
     </p>
     <p>
-        In this situation, assuming the size of your class is pretty small, it is easy to write a code.
-        But what if your class size is 300?
-        Also, sometimes programmers need to handle millions or billions of data, such as the average age of all Facebook users.
+        In this situation, assuming the size of our class is pretty small, it is easy to write a code.
+        But what if our class size is 300?
+        Sometimes programmers need to handle millions or billions of data, such as the average age of all Facebook users.
     </p>
     <p>
-        In these situations, using variables won't help much as the codes will go infinitely long.
-        And this is where array, one the data structures, comes in.
+        In these situations, using variables won't help much as the code will go infinitely long.
+        And this is where an array, one the data structures, comes in.
         With an array, we can rewrite the previous code as the following.
     </p>
     <p class='code'>
@@ -43,7 +48,7 @@
         int avg = sum / scores.length;
     </p>
 
-    <h2>What is Array?</h2>
+    <h2>What is an Array?</h2>
     <p>An array is a data structure that can store an ordered collection of the same type of elements with indexes.</p>
     <p>Take a look of the image below.</p>
     <img src="{{ URL::to('/img/array_drawing.png') }}">
@@ -51,26 +56,77 @@
         An array looks like a sequence of boxes. Each box is called an element.
         Elements are pretty much the same as variables--they have a type and can store data.
         In Java, an array can store only a single type of elements.
-        For example, if you create an array that can contain int type data, you can't store boolean type data into the array.
+        For example, if we create an array that can contain int type data, we can't store boolean type data into the array.
     </p>
     <p>
-        As you can see in the image above, each element in the array is associated with an number.
+        As shown in the image above, each element in the array is associated with an number.
         Each of these numbers is called an index. The index in an array always starts from 0.
     </p>
     <p>
-        One of the great benefits of using an array is that you can treat all of elements in an array as a single data.
-        This means that, with an array, you can do things like "sum up all the elements in the array" or
+        One of the great benefits of using an array is that we can treat all of elements in an array as a single data.
+        This means that, with an array, we can do things like "sum up all the elements in the array" or
         "find this value in the array" much easier than using variables.
     </p>
 
-    <h2>How to Use Array</h2>
-    <p></p>
+    <h2>How to Create an Array</h2>
+    <p>To create an array, there are 2 steps that we need to follow.</p>
+    <p>
+        Step 1: declare an array variable<br>
+        Step 2: initialize the array
+    </p>
+    <p>
+        Step 1: declare an array variable<br>
+        To declare an array, we need to specify the type of elements that can be stored in the array.
+        For example, the following code is a declaration of an array to store int types.
+    </p>
+    <p class='code'>
+        int[] scores;
+    </p>
+    <p>or</p>
+    <p class='code'>
+        int scores[];
+    </p>
+    <p>
+        We can use "double[] scores" or "double scores[]" to store double types,
+        and use "String[] names" or "String names[]" to store String types.
+    </p>
+    <p>
+        Step 2: initialize the array<br>
+        To initialize the array, we need to know how many elements we are going to put into the array because
+        an array's size is fixed after the initialization. For example, the following code will initialize the array
+        that we created above.
+    </p>
+    <p class='code'>
+        scores = new int[5];
+    </p>
+    <p>
+        In the example above, we cannot assign "new double[5]" or "new String[5]" because we declared
+        scores as an array to store int types already.
+    </p>
+    <p>
+        An array can be declared and initialized in a single line of code, like a variable can be.
+        To do so, we need to write:
+    </p>
+    <p class='code'>
+        int[] scores = new int[5];
+    </p>
+    <p>or</p>
+    <p class='code'>
+        int scores[] = new int[5];
+    </p>
+
+    <h2>How to Use an Array</h2>
 
     <h2>Summary</h2>
     <ul>
         <li><p>Array is a sequence of elements with indexes.</p></li>
         <li><p>In Java, an array can store only a single type of data.</p></li>
-        <li><p>The index in an array always start from 0.</p></li>
+        <li><p>The index in an array always starts from 0.</p></li>
+    </ul>
+
+    <h2>Coding Problems with Array</h2>
+    <ul>
+        <li></li>
     </ul>
 
 @endsection
